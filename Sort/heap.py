1901e70f6ac3e9a4809c6242cd04b415864a77dc
Papus <foal0912@gmail.com>
2022-04-05 22:56:52 +0900

    Counting Sort and Heap Sort

--- Sort/heap.py
+++ Sort/heap.py
@@ -4,6 +4,7 @@
 
 arr = [5, 11, 14, 3, 1, 9, 4, 6, 7, 2, 12, 10]
 
+# 최소 힙으로 만들기
 for i in range(len(arr)):
     d = i
     while d != 0:
@@ -13,10 +14,13 @@ for i in range(len(arr)):
             arr[u], arr[d] = arr[d], arr[u]
         
         d = u
-        
+
+# 힙 정렬 과정 
 for i in range(len(arr)-1, -1, -1):
+    # 맨 끝값과 첫 번째 값 바꾸기
     arr[0], arr[i] = arr[i], arr[0]
     
+    # 위에서부터 시작해 최소 힙으로 만들기
     u = d = 0
     while d < i:
         d = 2 * u + 1
